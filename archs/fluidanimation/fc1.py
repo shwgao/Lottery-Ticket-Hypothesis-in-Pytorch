07a842efb6d94511519701ee6583f21f83969c00
sgao014 <sgao014@fiu.edu>
2023-12-21 16:58:29 -0500

    dibugging

---
+++ archs/fluidanimation/fc1.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+class fc1(nn.Module):
+
+    def __init__(self, num_classes=5):
+        super(fc1, self).__init__()
+        self.classifier = nn.Sequential(
+            nn.Linear(20, 784),
+            nn.ReLU(inplace=True),
+            nn.Linear(784, 300),
+            nn.ReLU(inplace=True),
+            nn.Linear(300, 100),
+            nn.ReLU(inplace=True),
+            nn.Linear(100, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.classifier(x)
+        return x
